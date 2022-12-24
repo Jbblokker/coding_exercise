7b573f26fa1bb124c00c0ad77cc99f17bfa84f95
Jonathan Blokker <jbblokker@outlook.com>
2022-12-24 06:35:19 +0100

    functionality added
    Added the functionality to add new games and submit to the local storage.

--- exercise/src/components/new_game.js
+++ exercise/src/components/new_game.js
@@ -1,58 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+
 import './new.css'
-
 //external imports 
-import { CiSquarePlus } from 'react-icons/ci'
-import { CiSquareMinus } from 'react-icons/ci'
-
+import { GiGoalKeeper } from 'react-icons/gi'
 export class new_game extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          value: 0,
-          value2: 0,
+         
+          formValues: [{ name: "", number: "" }]
         };
+        this.handleSubmit = this.handleSubmit.bind(this)
       }
+
+      handleChange(i, e) {
+        let formValues = this.state.formValues;
+        formValues[i][e.target.name] = e.target.value;
+        this.setState({ formValues });
+      }
+
+      addFormFields() {
+        this.setState(({
+          formValues: [...this.state.formValues, {  name: "", number: "",name2: "", number2: ""}]
+        }))
+      }
+      
+       resetState () {
+        this.setState({
+        name: '',
+        name2: '',
+        number: '',
+        number2: '',
+        });
+      }
+
+      removeFormFields(i) {
+        let formValues = this.state.formValues;
+        formValues.splice(i, 1);
+        this.setState({ formValues });
+      }
+      
+      saveStateToLocalStorage = () => { 
+        const oldInfo = JSON.parse(localStorage.getItem('data'));
+        const newInfo = this.state.formValues
+        console.log(newInfo)
+        console.log(oldInfo)
+        localStorage.setItem('data', JSON.stringify({ oldInfo, newInfo}));
+      } 
+
+      handleSubmit(event) {
+        console.log(this.name)
+        event.preventDefault();
+        this.saveStateToLocalStorage();
+        event.preventDefault();
+        this.removeFormFields();
+     }
+
   render() {
-    return(
-    <div className='body'>
-        <h1 className='header'>New Games</h1>
-        <div className='newTeam'>
-            <form className='form_list' >
-              
-                    <label htmlFor='name'>Name</label>
-                    <input type ='text' id='name' placeholder='Team&#39;s name'/>
-                   
-                    <div onClick={ ()=> this .setState({value: this.state.value +1})}>
-                        <CiSquarePlus/>
-                    </div>
+    return (
+        <form className='form_list' onSubmit={this.handleSubmit}>
+          {this.state.formValues.map((element, index) => (
+            <div className='newTeam'>
+                <div className='home_team'>
+                    <label htmlFor='name'>
+                        Home Team
+                    </label> 
+                    <input type ='text' name="name" className='name_input'  placeholder='Team&#39;s name' 
+                        value={element.name || ""} onChange={e => this.handleChange(index, e)} />
+                    <label className='icon'>
+                        <GiGoalKeeper/>
+                    </label>
                     <div>
-                     {this.state.value} 
+                        <input type ='text' name='number'  className='number_input' placeholder='Score' 
+                        value={element.number || ""} onChange={e => this.handleChange(index, e)} />
                     </div>
-                    <div className='icon' onClick={ ()=> this .setState({value: this.state.value -1})}>
-                        <CiSquareMinus/>
-                    </div>
-               
-               
-                    <label htmlFor='name'>Name</label>
-                    <input type ='text' id='name' placeholder='Team&#39;s name'/>
-                
-              
-                    <div onClick={ ()=> this .setState({value2: this.state.value2 +1})}>
-                        <CiSquarePlus/>
-                    </div>
-                    <div>
-                     {this.state.value2} 
-                    </div>
-                    <div onClick={ ()=> this .setState({value2: this.state.value2 -1})}>
-                        <CiSquareMinus/>
-                    </div>
-             
-           
-            </form>   
-        </div>
-    </div>
-    )
+                </div>
+                <div className='away_team'>
+                <label htmlFor='name'>
+                    Away Team
+                </label>
+                <input type ='text' name="name2" className='name_input' placeholder='Team&#39;s name'
+                    value={element.name2 || ""} onChange={e => this.handleChange(index, e)}/>
+                <label className='icon' >
+                    <GiGoalKeeper/>
+                </label>
+                <div>
+                <input type ='text' name='number2' className='number_input' placeholder='Score' 
+                    value={element.number2 || ""} onChange={e => this.handleChange(index, e)} /> 
+                </div> 
+                </div>
+                <button className="button_submit" type="submit">Submit</button>
+                {
+                index ? 
+                  <button type="button"  className="button_remove" onClick={() => this.removeFormFields(index)}>Remove</button> 
+                : null
+              }
+            </div>
+          ))}
+          <div className="button_section">
+              <button className="button_add" type="button" onClick={() => this.addFormFields()}>Add</button>
+          </div>
+      </form>
+    );
   }
 }
 export default new_game;
+
+
